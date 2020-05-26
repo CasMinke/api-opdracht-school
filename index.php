10e0ba6131d3502de17f232e7cb34ddf7f8c07d0
CasMinke <44709992+CasMinke@users.noreply.github.com>
2020-05-26 09:36:39 +0200

    versie 1
    buttons toegevoegt versie 1

--- index.php
+++ index.php
@@ -19,11 +19,152 @@
     </style>
 </head>
 <body>
+<div class="buttons">
+<button onclick="nederland()">nederland</button>
+<button onclick="chernobyl()">chernobyl</button>
+<button onclick="timessquare()">times square new york</button>
+<button onclick="mountrushmore()">mountrushmore</button>
+<button onclick="Nürburgring()">Nürburgring</button>
+<button onclick="moskou()">moskou</button>
+<button onclick="Hiroshima()">Hiroshima</button>
+<button onclick="hell()">hell noorwegen</button>
+</div>
 <div id="map"></div>
 <script>
+var lati = 52.370216
+var lang = 4.895168
+
+function nederland(){
+	lati = 52.370216;
+	lang = 4.895168;
+	var myLatLng = {lat: lati, lng: lang};
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 4,
+            center: myLatLng
+        });
+
+        var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: map,
+            title: 'Hello World!'
+        });
+}
+function chernobyl(){
+	lati = 51.3870;
+	lang = 30.0927;
+	var myLatLng = {lat: lati, lng: lang};
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 4,
+            center: myLatLng
+        });
+
+        var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: map,
+            title: 'Hello World!'
+        });
+}
+function timessquare(){
+	lati = 40.758896;
+	lang = -73.985130;
+	var myLatLng = {lat: lati, lng: lang};
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 4,
+            center: myLatLng
+        });
+
+        var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: map,
+            title: 'Hello World!'
+        });
+}
+function mountrushmore(){
+	lati = 43.9685522;
+	lang = -103.38183449999997;
+	var myLatLng = {lat: lati, lng: lang};
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 4,
+            center: myLatLng
+        });
+
+        var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: map,
+            title: 'Hello World!'
+        });
+}
+function Nürburgring(){
+	lati = 50.334098;
+	lang = 6.942662;
+	var myLatLng = {lat: lati, lng: lang};
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 4,
+            center: myLatLng
+        });
+
+        var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: map,
+            title: 'Hello World!'
+        });
+}
+function moskou(){
+	lati = 55.751244;
+	lang = 37.618423;
+	var myLatLng = {lat: lati, lng: lang};
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 4,
+            center: myLatLng
+        });
+
+        var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: map,
+            title: 'Hello World!'
+        });
+}
+function Hiroshima(){
+	lati = 34.383331;
+	lang = 132.449997;
+	var myLatLng = {lat: lati, lng: lang};
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 4,
+            center: myLatLng
+        });
+
+        var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: map,
+            title: 'Hello World!'
+        });
+}
+function hell(){
+	lati = 63.4333 ;
+	lang = 10.9000;
+	var myLatLng = {lat: lati, lng: lang};
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 4,
+            center: myLatLng
+        });
+
+        var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: map,
+            title: 'Hello World!'
+        });
+}
 
     function initMap() {
-        var myLatLng = {lat: -25.363, lng: 131.044};
+        var myLatLng = {lat: lati, lng: lang};
 
         var map = new google.maps.Map(document.getElementById('map'), {
             zoom: 4,
